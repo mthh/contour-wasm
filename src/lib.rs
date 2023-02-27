5eacf59448ace9593a552acd43af28b92cd56ab8
Matthieu Viry <matthieu.viry@cnrs.fr>
2023-02-27 16:58:58 +0100

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+use contour_isobands::isobands;
+use geojson::{GeoJson, FeatureCollection};
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn make_contours(data: &[f64], lx: usize, ly: usize, intervals: &[f64]) -> String {
+    utils::set_panic_hook();
+    let mut _data:Vec<Vec<f64>> = Vec::new();
+    for i in 0..lx {
+        let mut row = Vec::new();
+        for j in 0..ly {
+            row.push(data[i * ly + j]);
+        }
+        _data.push(row);
+    }
+    let res = isobands(&_data, intervals).unwrap();
+    let features = res.iter()
+        .map(|band| band.to_geojson())
+        .collect::<Vec<geojson::Feature>>();
+    GeoJson::from(FeatureCollection {
+        bbox: None,
+        features,
+        foreign_members: None,
+    }).to_string()
+}
