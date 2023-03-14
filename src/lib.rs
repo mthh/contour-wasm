bfcada1e9da1f556e43405a57c7ad8d084eebc65
Matthieu Viry <matthieu.viry@univ-grenoble-alpes.fr>
2023-03-14 08:08:18 +0100

    Improve the API exposed in JS

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod utils;
 
 use contour_isobands::ContourBuilder;
 use geojson::{Feature, FeatureCollection, GeoJson};
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -10,28 +11,74 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Deserialize)]
+struct Options {
+    use_quad_tree: Option<bool>,
+    x_origin: Option<f64>,
+    y_origin: Option<f64>,
+    x_step: Option<f64>,
+    y_step: Option<f64>,
+}
+
+// #[wasm_bindgen]
+// extern "C" {
+//     #[wasm_bindgen(js_namespace = console)]
+//     fn log(s: &str);
+// }
+//
+// macro_rules! console_log {
+//     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+// }
+
 #[wasm_bindgen(js_name = "makeContours")]
 pub fn make_contours(
     data: &[f64],
     width: usize,
     height: usize,
     intervals: &[f64],
-    use_quad_tree: bool,
-) -> Result<String, JsValue> {
+    options: &JsValue,
+) -> Result<JsValue, JsError> {
     utils::set_panic_hook();
+    let (use_quad_tree, x_origin, y_origin, x_step, y_step) =
+        if options.is_null() || options.is_undefined() {
+            (true, 0.0, 0.0, 1.0, 1.0)
+        } else {
+            let options: Options = serde_wasm_bindgen::from_value(options.into())?;
+            (
+                options.use_quad_tree.unwrap_or(true),
+                options.x_origin.unwrap_or(0.0),
+                options.y_origin.unwrap_or(0.0),
+                options.x_step.unwrap_or(1.0),
+                options.y_step.unwrap_or(1.0),
+            )
+        };
 
     let features: Vec<Feature> = ContourBuilder::new(width, height)
         .use_quad_tree(use_quad_tree)
+        .x_origin(x_origin)
+        .y_origin(y_origin)
+        .x_step(x_step)
+        .y_step(y_step)
         .contours(data, intervals)
         .map_err(|err| JsError::new(&format!("Failed to build contours: {}", err)))?
         .iter()
         .map(|band| band.to_geojson())
         .collect::<Vec<geojson::Feature>>();
 
-    Ok(GeoJson::from(FeatureCollection {
+    let s = serde_wasm_bindgen::Serializer::new().serialize_maps_as_objects(true);
+
+    let res = GeoJson::from(FeatureCollection {
         bbox: None,
         features,
         foreign_members: None,
     })
-    .to_string())
+    .serialize(&s)
+    .map_err(|err| {
+        JsError::new(&format!(
+            "Failed to transform GeoJSON feature collection to JS Object: {}",
+            err
+        ))
+    })?;
+
+    Ok(res)
 }
--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,28 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+
+use contour_isobands_wasm::make_contours;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn make_contours_works() {
+    let data = vec![1., 1., 1., 5., 1., 1., 5., 5., 1.];
+    let width = 3;
+    let height = 3;
+    let intervals = vec![2., 5.];
+    let use_quad_tree = false;
+
+    let res = make_contours(&data, width, height, &intervals, use_quad_tree);
+
+    // let expected = JsValue::from_str(r#"{"features":[{"geometry":{"coordinates":[[[[0.75,1],[0,0.25],[0,1],[0,2],[1,2],[1.75,2],[1,1.25],[0.75,1]]]],"type":"MultiPolygon"},"properties":{"max_v":5,"min_v":2},"type":"Feature"}],"type":"FeatureCollection"}"#);
+
+    assert!(res.is_ok());
+    // assert_eq!(
+    //     res.ok().expect("failed"),
+    //     expected
+    // );
 }
